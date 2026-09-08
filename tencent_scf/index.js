1f6c0e1cec31ae96b05f3ad82cc2a5d0fc57a3f9
Yummy-He <Yummy-He@users.noreply.github.com>
2026-09-08 23:28:34 +0800

    scf: add isBase64Encoded:false to integrated response

--- tencent_scf/index.js
+++ tencent_scf/index.js
@@ -199,6 +199,7 @@ async function writeTrigger(env, ts) {
 
 function jsonResp(status, obj) {
   return {
+    isBase64Encoded: false,
     statusCode: status,
     headers: { 'Content-Type': 'application/json; charset=utf-8' },
     body: JSON.stringify(obj),
