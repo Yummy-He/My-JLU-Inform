6e4bfdf603baee55af67d97697543fda976a549d
Yummy-He <Yummy-He@users.noreply.github.com>
2026-09-08 23:42:36 +0800

    scf: drop isBase64Encoded for function URL response

--- tencent_scf/index.js
+++ tencent_scf/index.js
@@ -199,7 +199,6 @@ async function writeTrigger(env, ts) {
 
 function jsonResp(status, obj) {
   return {
-    isBase64Encoded: false,
     statusCode: status,
     headers: { 'Content-Type': 'application/json; charset=utf-8' },
     body: JSON.stringify(obj),
